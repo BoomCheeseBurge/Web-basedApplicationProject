f4e0875c9e020855177f260e3b8f6598f9e6210d
Brandon John <80831628+BoomCheeseBurge@users.noreply.github.com>
2024-08-08 21:47:33 +0700

    Update index.php

--- studentgrant/phpmailer/index.php
+++ studentgrant/phpmailer/index.php
@@ -20,19 +20,19 @@ $mail->SMTPSecure = "tls";
 //Set port to connect smtp
 $mail->Port = "587";
 //Set gmail username
-$mail->Username = "brandonjohnlyc@gmail.com";
+$mail->Username = "johndoe@gmail.com";
 //Set gmail password
-$mail->Password = "nlcnbhywfadhizkd";
+$mail->Password = "";
 //Set email sucject
 $mail->Subject = "Test Email Using PHPMailer";
 //Set email sender
-$mail->setFrom("brandonjohnlyc@gmail.com");
+$mail->setFrom("johndoe@gmail.com");
 //Enable HTML
 $mail->isHTML(true);
 //Email body
 $mail->Body = "<h1>This is HTML h1 Heading</h1><br>";
 //Add recipient
-$mail->addAddress("brandonjohnlyc@gmail.com");
+$mail->addAddress("johndoe@gmail.com");
 //Finally send email
 if ($mail->send()) {
     echo "Email sent...!";
